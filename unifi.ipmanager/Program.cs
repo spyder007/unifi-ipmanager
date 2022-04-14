b45def043941932c46ec3cb67219b973d70d4448
Matt Gerega <gerega@gmail.com>
2022-04-13 22:50:01 -0400

    Updated startup and changed healthcheck endpoint

--- unifi.ipmanager/Program.cs
+++ unifi.ipmanager/Program.cs
@@ -26,6 +26,7 @@ try
 
     var startup = new Startup(builder.Configuration);
     startup.ConfigureServices(builder.Services);
+
     var app = builder.Build();
     startup.Configure(app, app.Environment);
 
